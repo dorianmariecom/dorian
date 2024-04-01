556f09a13724eea96edb965dd504eed80c8bbb26
Dorian Marié <dorian@dorianmarie.com>
2024-04-01 00:52:44 -0700

    add tests and binstubs

---
+++ spec/dorian_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe "Dorian" do
+  it "has the dorian-times gem" do
+    expect(`bin/times 3`).to eq("1\n2\n3\n")
+  end
+
+  it "has the dorian-each gem" do
+    expect(`bin/times 2 | each "puts it.to_i * 2"`).to eq("2\n4\n")
+  end
+end
